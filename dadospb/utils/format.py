298957e1c2ad1e6fb4cdb0798b1c7bdd30969bfd
Vinicius <vinicius.matheus252@gmail.com>
2019-03-09 13:26:59 -0300

    Add period formater

--- dadospb/utils/format.py
+++ dadospb/utils/format.py
@@ -3,3 +3,9 @@ def format_month(month):
         return "0" + str(month)
 
     return str(month)
+
+def format_period(start_month, start_year, end_month, end_year):
+    if start_month == end_month and start_year == end_year:
+        return f'{format_month(start_month)}{start_year}'
+    else:
+        return f'{format_month(start_month)}{start_year}_{format_month(end_month)}{end_year}'
