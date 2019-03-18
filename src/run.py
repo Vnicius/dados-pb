b060815a93c02d303deca2fb2b381852ecbc014d
Vinicius <vinicius.matheus252@gmail.com>
2019-03-17 22:55:42 -0300

    Add arguments in the download call

--- src/run.py
+++ src/run.py
@@ -18,21 +18,14 @@ def main(args):
     files = [ f for f in files if not f in IGNORE_FILES ]
     
     # definir o diretôrio de saída
-    output_dir = ''
-
-    if args.output:
-        output_dir = args.output
-    else:
-        output_dir = f'data_{dt.now().strftime("%d-%m-%Y")}'
+    if not args.output:
+        args.output = f'data_{dt.now().strftime("%d-%m-%Y")}'
 
     # executar os scripts
     for f in files:
         module_name = f.replace('.py', '')
         module = __import__(module_name)
-        module.Download(start_year=args.year, start_month=args.month,
-             end_year=args.untilyear, end_month=args.untilmonth,
-             merge_data=args.merge, file_type=args.format,
-             output_dir=output_dir).download()
+        module.Download(args).download()
 
 if __name__ == '__main__':
     dargs = DownloadArgs()
